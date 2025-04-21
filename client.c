358966d5c62a62f35e9049fd94dcd4243bb3e500
Gabby <gvadai91@icloud.com>
2025-04-21 09:28:41 -0500

    Initial commit

---
+++ client.c
@@ -0,0 +1,127 @@
+#include "csapp.h"
+
+#define BUFFSIZE 1024
+//Global variables 
+int socket_fd;  //file descriptor to communicate with the server
+char username[50]; //username
+
+//Function prototypes
+void *receiver(void *arg);
+void sendMessage(const char *message);
+
+    
+
+int main(int argc, char *argv[])
+{
+   
+    char *host, *port;
+    
+    char buffer[MAXLINE]; //MAXLINE = 8192 defined in csapp.h
+    size_t n
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+	   exit(0);
+    }
+
+    host = argv[1];
+    port = argv[2];
+
+    socket_fd = Open_clientfd(host, port); //wrapper function that calls getadderinfo, socket and connect functions for client side
+
+    //Get the welcome message from the server
+    n = read(socket_fd, buffer, sizeof(buffer));
+    if (n < 0) {
+        fprintf(stderr, "failed to receive welcome message\n");
+        exit(1);
+    }
+
+    buffer[n] = '\0'; // null-terminate the string
+    printf("%s\n", buffer); // print the welcome message
+
+    // Get username from the user
+    printf("Enter your username: ");
+    Fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = '\0'; // remove newline character
+
+
+    // Send username to the server
+    n = write(socket_fd, username, strlen(username));
+    if (n < 0) {
+        fprintf(stderr, "failed to send username\n");
+        exit(1);
+    }
+    //Parallel thread to receive messages
+    pthread_t tid;
+    pthread_create(&tid, NULL, receiver, NULL);
+
+    // Main loop to send messages to the server
+    while (1){
+        printf("> ");
+        fflush(stdout); // flush stdout to ensure prompt is displayed
+        Fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0'; // remove newline character
+        if (strcmp(buffer, "exit") == 0) {
+            sendMessage("exit");
+            break; // exit the loop
+        }
+
+        sendMessage(buffer);  //Call send message to send to server
+        
+    }
+    close(socket_fd); // close the socket
+
+    return 0;
+}
+
+// Function to receive messages from the server
+void *receiver(void *arg)
+{
+    char buffer[BUFFSIZE];
+    size_t n;
+    // Initialize the file descriptor set
+    fd_set readfds;
+    struct timeval timeout; // timeout for select function
+    
+    while (1)
+    {
+        FD_ZERO(&readfds); //Clear readfds
+        FD_SET(socket_fd, &readfds);  //Add fd to readfds
+
+        timeout.tv_sec = 1; // set timeout to 1 second
+        timeout.tv_usec = 0; // set microseconds to 0
+
+        //call select function to check if there is any data to read
+        int activity = select(socket_fd + 1, &readfds, NULL, NULL, &timeout);
+        if (activity < 0)
+        {
+            printf("select error\n");
+            continue;
+        }
+        //if the socket_fd was set, read the data into buffer
+        if(FD_ISSET(socket_fd, &readfds))
+        {
+            n = read(socket_fd, buffer, sizeof(buffer));
+            if (n <= 0)
+            {
+                printf("Disconnected from server\n");
+                exit(0);
+            }
+            buffer[n] = '\0'; // null-terminate the string
+            printf("\n%s\n> ", buffer); // print the message
+            fflush(stdout); // flush stdout to ensure prompt is displayed
+        }
+    }
+}
+// Function to send message to the server
+void sendMessage(const char *message)
+{
+    size_t n = write(socket_fd, message, strlen(message));
+    if (n < 0)
+    {
+        fprintf(stderr, "failed to send message\n");
+        exit(1);
+    }
+}
+// Function to receive message from the server
---
+++ server.c
@@ -0,0 +1,143 @@
+#include "csapp.h"
+
+//max size of the file and length of the string
+#define MAX_CLIENT 10   
+#define MAXSIZE 1024 
+
+// Define struct to hold employee information from file
+typedef struct {
+    int connfd; // file descriptor for the client connection
+    char username[50]; // username of the client
+} client_t;
+
+//Global Variables
+client_t * clients[MAX_CLIENT]; // array of client_t structs to hold client information
+pthread_mutex_t clients_mutex; // mutex for synchronizing access to clients array
+
+//function prototypes
+void *handle_client(void *arg);
+void broadcast(char *message, int sender_fd);
+void add_client(client_t *client);
+void remove_client(int connfd);
+
+
+
+int main(int argc, char *argv[])
+{
+    int listenfd, connfd; 
+    int *connfdp;
+     //file descriptor to communicate with the client
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;  /* Enough space for any address */
+    
+    
+
+    if (argc != 2) {
+	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	exit(0);
+    }
+
+    listenfd = Open_listenfd(argv[1]); //wrapper function that calls getadderinfo, socket, bind, and listen functions in the server side
+
+    //Server runs in the infinite loop.
+    //To stop the server process, it needs to be killed using the Ctrl+C key.
+    while (1) {
+    	clientlen = sizeof(struct sockaddr_storage);
+
+        //Accept client connection
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        connfdp = Malloc(sizeof(int));
+        *connfdp = connfd;
+
+        pthread_t client_thread;
+        pthread_create(&client_thread,NULL,handle_client,(void *)connfdp);            
+    }
+    return 0;
+}
+//Thread for concurrency
+void *handle_client(void *arg){
+    int connfd = *((int *)arg); // retrieve client socket address
+    free(arg); //free memory
+    char buffer[MAXSIZE];
+    size_t n;
+
+    //send welcome message
+    char welcome_message[MAXSIZE] = "Welcome to the chatroom.\nPlease Remember to always be kind and curtious to other members.\n";
+    write(connfd, welcome_message, strlen(welcome_message));
+
+    //revceive the username
+    if((n = read(connfd,buffer, sizeof(buffer)) <= 0)){
+        close(connfd);
+        return NULL;
+    }
+    
+    char username[50];
+    strcpy(username, buffer); //copy the username to the username variable
+
+    //create a new client struct and add it to the clients array
+    client_t *client = malloc(sizeof(client_t));
+    client->connfd = connfd; //sore the client connfd
+    strcpy(client->username, username); //store the client username
+    add_client(client); //add the client to the clients array
+
+    // Inform the users the a new user has joined
+    char join_message[MAXLINE];
+    sprintf(join_message, "%s has joined the chatroom.\n", username); //prepare the message
+    broadcast(join_message, connfd); //broadcast the message to all clients
+
+    // Listen for other clients messages
+    while ((n = read(connfd, buffer, sizeof(buffer))) > 0) {
+        buffer[n] = '\0'; // Null terminate the received message
+        if(strcmp(buffer, "exit") == 0) {
+            break; // exit the loop if the client sends "exit"
+        }
+        // Broadcast the message to all clients
+        char message[MAXLINE];
+        sprintf(message, "%s: %s", username, buffer); //prepare the message
+        broadcast(message, connfd); //broadcast the message to all clients
+        
+    }
+    
+
+    // Client disconnected
+    remove_client(connfd); //remove the client from the clients array
+
+    // Inform the users that the client has left
+    char leave_message[MAXLINE];
+    sprintf(leave_message, "%s has left the chatroom.\n", username); //prepare the message
+    broadcast(leave_message, connfd); //broadcast the message to all clients
+
+    close(connfd); //close the client connection
+    return NULL;
+
+}
+void broadcast(char *message, int sender_fd) {
+    pthread_mutex_lock(&clients_mutex); //lock the mutex
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (clients[i] != NULL && clients[i]->connfd != sender_fd) {
+            write(clients[i]->connfd, message, strlen(message)); //send the message to all clients except the sender
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex); //unlock the mutex
+}
+void add_client(client_t *client) {
+    pthread_mutex_lock(&clients_mutex); //lock the mutex
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (clients[i] == NULL) {
+            clients[i] = client; //add the client to the clients array
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex); //unlock the mutex
+}
+void remove_client(int connfd) {
+    pthread_mutex_lock(&clients_mutex); //lock the mutex
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (clients[i] != NULL && clients[i]->connfd == connfd) {
+            free(clients[i]); //free the memory allocated for the client
+            clients[i] = NULL; //remove the client from the clients array
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex); //unlock the mutex
+}
